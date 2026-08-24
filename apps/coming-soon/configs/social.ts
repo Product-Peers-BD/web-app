b8e5982383e8360e640da43af76d6259304b726b
zahidshowrav <zahid.showravbd@gmail.com>
2026-08-24 23:01:10 +0600

    feat: implemented social icons, updated social links and site contact email, and refined UI for the landing page in coming soon app

--- apps/coming-soon/configs/social.ts
+++ apps/coming-soon/configs/social.ts
@@ -1,7 +1,10 @@
 import type { SocialLink } from '@/types/nav';
 
 export const socialLinks: SocialLink[] = [
-	{ label: 'LinkedIn', href: 'https://linkedin.com' },
-	{ label: 'Facebook', href: 'https://facebook.com' },
-	{ label: 'YouTube', href: 'https://youtube.com' }
+	{
+		label: 'LinkedIn',
+		href: 'https://www.linkedin.com/company/productpeers-bd/'
+	},
+	{ label: 'Facebook', href: 'https://www.facebook.com/productpeers.bd' },
+	{ label: 'YouTube', href: 'https://www.youtube.com/@ProductPeersBD' }
 ];
